d8546004bb1c0d02c8e4e32126c0bd350e600cd0
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-12 16:58:06 -0600

    problem 1 finito

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      typing: ''
+    }
+  }
+
+
+  handleTyping = (stuff) => {
+    this.setState({
+      typing: stuff
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={ (e) => this.handleTyping(e.target.value) }/>
+        <p>{this.state.typing}</p>
       </div>
     );
   }
