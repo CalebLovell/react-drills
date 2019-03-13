5779f0aab394f86ac3a58b98f079d49cb1c26563
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-03-12 18:57:58 -0600

    prob 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+      list: ['chair', 'cheese', 'cow', 'dog'],
+      filteredList: [],
+    }
+  }
+
+  handleChange = (evt) => {
+    this.setState({
+      text: evt.target.value,
+    })
+  }
+  
+
+  filterList = () => {
+    let {text, list} = this.state;
+    let filter = list.filter((word) => word.includes(text)).map((word) => word)
+    return filter
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={this.handleChange} />
+        <h2>{this.filterList()}</h2>
       </div>
     );
   }
